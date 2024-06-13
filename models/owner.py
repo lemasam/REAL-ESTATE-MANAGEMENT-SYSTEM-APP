1dab419e103f946826d5e5b90fa3ede0133ee192
lema samuel <lemasam10@gmail.com>
2024-06-13 10:24:21 +0300

    creating owner table

--- models/owner.py
+++ models/owner.py
@@ -12,7 +12,45 @@ class Owner:
         self.id = id 
         self.name = name
         self.phone_number = phone_number
-
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter 
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
+    
+    
+    @property
+    def phone_number(self):
+        return self._phone_number
+    
+    @phone_number.setter
+    def name(self, phone_number):
+        if isinstance(phone_number, int) and len(phone_number):
+            self._phone_number = phone_number
+        else:
+            raise ValueError(
+                "Name must be a non-empty integer"
+            )
+            
+    @classmethod
+    def create_table(cls):
+        sql = """
+        CREATE TABLE  IF NOT EXISTS owners(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            phone_number INTEGER
+        )
+        """
+        
+   
 
 
 
