790e1f9f077eef44b4ff5c73e132ac57f4c432a0
lema samuel <lemasam10@gmail.com>
2024-06-13 10:50:21 +0300

    creating proprty table

--- models/property.py
+++ models/property.py
@@ -12,4 +12,29 @@ class Property:
     @property
     def address(self):
         return self._address
-  
+    
+    @address.setter
+    def address(self,address):
+        if isinstance(address,str) and len(address):
+            self._address = address
+            
+        else: 
+            raise ValueError(
+                "Address must be a non-empty string"
+            )
+            
+    @property
+    def owner_id(self):
+        return self._owner_id 
+    
+    @owner_id.setter
+    def owner_id(self, owner_id):
+        if type(owner_id) is int and Owner.find_by_id(owner_id):
+            self._owner_id = owner_id
+            
+        else:
+            raise ValueError(
+                " owner_id must reference a owner in the database"
+            )
+    
+    @
